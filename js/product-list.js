bfdd00718b8633f43f059f23538ef7e65936174b
Kalym-Oleksandr <Kalymsasha@gmail.com>
2022-05-30 12:34:55 +0000

    add

--- js/product-list.js
+++ js/product-list.js
@@ -1,5 +1,5 @@
 (async function () {
-    
+
     function renderProducts(products) {
         const productsContainer = document.querySelector('.multiple-items');
         for (const product of products) {
@@ -14,9 +14,24 @@
         }
     }
 
-    const response = await fetch('products.json');
+    const response = await fetch('/products.json');
     const products = await response.json();
 
     renderProducts(products);
 
+    const slides = document.querySelectorAll('.multiple-items-cabernet');
+    let currentSlide = 0;
+
+    function renderCarrousel() {
+        const carrouselContainer = document.querySelector('.main-products-carrousel');
+        carrouselContainer.innerHTML = slides[currentSlide].outerHTML;
+    }
+
+    function nextSlide() {
+        currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+        renderCarrousel();
+    }
+
+    setInterval(nextSlide, 3000);
+    renderCarrousel();
 })();
